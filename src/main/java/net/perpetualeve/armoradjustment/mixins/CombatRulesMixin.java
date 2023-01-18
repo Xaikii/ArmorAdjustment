a4ffaec7f77d5e0aaf9192a199aeb2b34ea2a1b2
Xaikii <xaikii@outlook.de>
2023-01-18 16:53:52 +0100

    1.16

--- src/main/java/net/perpetualeve/armoradjustment/mixins/CombatRulesMixin.java
+++ src/main/java/net/perpetualeve/armoradjustment/mixins/CombatRulesMixin.java
@@ -4,7 +4,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.CombatRules;
 import net.perpetualeve.armoradjustment.ArmorAdjustment;
 
@@ -13,8 +12,8 @@ public class CombatRulesMixin {
 
 	@Redirect(method = "getDamageAfterAbsorb", at = @At("Invoke"))
 	private static float getDamageAfterAbsorbRed(float u1, float u2, float u3, float damage, float armor, float toughness) {
-		float sqrt = Mth.fastInvSqrt(damage);
-		float div = sqrt+Mth.fastInvSqrt(armor*((toughness/ArmorAdjustment.toughness)*sqrt));
+		float sqrt = (float) Math.sqrt(damage);
+		float div = (float) (sqrt+Math.sqrt(armor+((toughness/ArmorAdjustment.toughness)*sqrt)));
 		return damage*(sqrt/div);
 	}
 }
