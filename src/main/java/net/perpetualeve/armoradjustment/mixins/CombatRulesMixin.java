d912bdca762c5aa6519e37799e1800320002c16b
Xaikii <xaikii@outlook.de>
2023-01-18 20:05:54 +0100

    1.19 fix, again again

--- src/main/java/net/perpetualeve/armoradjustment/mixins/CombatRulesMixin.java
+++ src/main/java/net/perpetualeve/armoradjustment/mixins/CombatRulesMixin.java
@@ -2,7 +2,9 @@ package net.perpetualeve.armoradjustment.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.world.damagesource.CombatRules;
 import net.perpetualeve.armoradjustment.ArmorAdjustment;
@@ -10,11 +12,10 @@ import net.perpetualeve.armoradjustment.ArmorAdjustment;
 @Mixin(CombatRules.class)
 public class CombatRulesMixin {
 
-	@Redirect(method = "getDamageAfterAbsorb", at = @At("Invoke"))
-	private static float getDamageAfterAbsorbRed(float u1, float u2, float u3, float damage, float armor, float toughness) {
+	@Inject(method = "getDamageAfterAbsorb", at = @At("Invoke"), cancellable = true, locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+	private static void getDamageAfterAbsorbRed(float damage, float armor, float toughness, CallbackInfoReturnable<Float> ci) {
 		double sqrt = Math.sqrt(damage);
-		double div = sqrt+Math.sqrt(armor+((toughness/ArmorAdjustment.toughness)*sqrt));
-		System.out.println(damage + " : " + armor  + " : " + toughness + " : " + (damage*(sqrt/div)));
-		return (float) (damage*(sqrt/div));
+		double div = (sqrt+Math.sqrt(armor+((toughness/ArmorAdjustment.toughness)*sqrt)));
+		ci.setReturnValue((float) (damage*(sqrt/div)));
 	}
 }
