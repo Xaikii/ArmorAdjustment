a6bae703d1021d5dc3dd7dcc3e5f78633f5deabf
Xaikii <xaikii@outlook.de>
2023-02-13 14:11:41 +0100

    1.18 Update

--- src/main/java/net/perpetualeve/armoradjustment/ArmorAdjustment.java
+++ src/main/java/net/perpetualeve/armoradjustment/ArmorAdjustment.java
@@ -13,14 +13,27 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ArmorAdjustment {
 	public static final String MODID = "armoradjustment";
 	
-	public static float toughness;
+	public static float armor_div;
+	DoubleValue ArmorDividor;
+	public static float toughness_div;
 	DoubleValue ToughnessDividor;
+	public static float armor_base;
+	DoubleValue ArmorBaseCFG;
+	public static float toughness_base;
+	DoubleValue ToughnessBaseCFG;
 	public static ForgeConfigSpec CONFIG;	
 	
 	public ArmorAdjustment() {
 		ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 		builder.push("general");
-		ToughnessDividor = builder.defineInRange("toughness_dividor", 12D, 0.01D, Double.MAX_VALUE);
+		builder.comment("the armor will be divided by this number in calculation");
+		ArmorDividor = builder.defineInRange("armor_dividor", 1D, 0.01D, Double.MAX_VALUE);
+		builder.comment("the toughness will be divided by this number in calculation");
+		ToughnessDividor = builder.defineInRange("toughness_dividor", 1D, 0.01D, Double.MAX_VALUE);
+		builder.comment("the base used for armor in calculation");
+		ArmorBaseCFG = builder.defineInRange("armor_base", 0.966D, 0.0D, Double.MAX_VALUE);
+		builder.comment("the base used for toughness in calculation");
+		ToughnessBaseCFG = builder.defineInRange("toughness_base", 0.99D, 0.0D, Double.MAX_VALUE);
 		builder.pop();
 		CONFIG = builder.build();
 		ModLoadingContext.get().registerConfig(Type.COMMON, CONFIG, "ArmorAdjustment.toml");
@@ -32,11 +45,17 @@ public class ArmorAdjustment {
 	
 	public void onLoad(ModConfigEvent.Loading configEvent) 
     {
-    	toughness = ToughnessDividor.get().floatValue();
+    	armor_div = ArmorDividor.get().floatValue();
+    	toughness_div = ToughnessDividor.get().floatValue();
+    	armor_base = ArmorBaseCFG.get().floatValue();
+    	toughness_base = ToughnessBaseCFG.get().floatValue();
     }
 
     public void onFileChange(ModConfigEvent.Reloading configEvent) 
     {
-    	toughness = ToughnessDividor.get().floatValue();
+    	armor_div = ArmorDividor.get().floatValue();
+    	toughness_div = ToughnessDividor.get().floatValue();
+    	armor_base = ArmorBaseCFG.get().floatValue();
+    	toughness_base = ToughnessBaseCFG.get().floatValue();
     }
 }
